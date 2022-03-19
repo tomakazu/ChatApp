50b0416fd15c4a1739e168eacbf9464d7f863e79
Mesut Ozil <matcortnermat@gmail.com>
2022-03-19 05:45:50 +0530

    Basic challenges completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Rishabh", password: "password")
+User.create(username: "Elizabeth", password: "password")
+User.create(username: "Botez", password: "password")
+User.create(username: "Kiernan", password: "password")
+User.create(username: "Daniell", password: "password")
+
+Message.create(body: "Hi, handsome!", user_id: 2)
+Message.create(body: "Hey", user_id: 1)
+Message.create(body: "You are hot", user_id: 4)
+Message.create(body: "Thanks", user_id: 1)
+Message.create(body: "Wanna meet us?", user_id: 4)
