4ae67c92b4865b7e8c882db3169fc5f982c46abe
Mesut Ozil <matcortnermat@gmail.com>
2022-03-20 14:04:56 +0530

    Completed!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,19 @@
 //= require turbolinks
 //= require_tree .
 
-scroll_bottom = function(){
+scroll_bottom = function() {
     if ($('#messages').length > 0){
         $('#messages').scrollTop($('#messages')[0].scrollHeight)
     }
+}
+
+submit_message = function() {
+    $('#message_body').on('keydown', function(e){
+        if(e.keyCode == 13){
+            $('button').click();
+            e.target.value = "";       
+        }
+    }) 
 }
 
 $(document).on('turbolinks:load', function(){
@@ -29,6 +38,6 @@ $(document).on('turbolinks:load', function(){
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
-
     scroll_bottom();
+    submit_message();
 })
