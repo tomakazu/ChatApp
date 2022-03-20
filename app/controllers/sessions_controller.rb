17e59734b92c83c2c35ea9763fa850a1f240454a
Mesut Ozil <matcortnermat@gmail.com>
2022-03-20 12:30:38 +0530

    ActionCable added plus scroll is added to the chat

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,32 @@
 class SessionsController < ApplicationController
+
+  before_action :logged_in_redirect, only: [:create, :new]
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session]["username"])
+    if user && user.authenticate(params[:session]["password"])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome "+user.username
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something is wrong in your credentails"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully log out"
+    redirect_to login_path
+  end
+
+  private
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  post 'login', to: 'sessions#create'
   get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chatroom#index'
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
